74bd61a3e2ee7e7e3cce6b1840a85cec7ef0ecbc
Ian Townend <iantownend@outlook.com>
2024-11-17 17:15:47 +0000

    Update tableviewer_user_plots.m
    tableviewer_user_plots - improved handling of user cancelling selection. scatter_plot modified to get_selected_text and a button added to display the Selection. Tested with multi-data type tables.

--- app/tableviewer_user_plots.m
+++ app/tableviewer_user_plots.m
@@ -52,16 +52,17 @@ function scatter_plot(mobj)
 
     %prompt user to select variables to be used in plot
     promptxt = 'Select X-variable:';    
-    indvar = get_variable(mobj,promptxt);
+    [indvar,indsel] = get_selection(mobj,promptxt,'XYZmxvar',1);
+    if isempty(indvar), return; end
     promptxt = 'Select Y-variable:';    
-    depvar = get_variable(mobj,promptxt);
-    if isempty(indvar) || isempty(depvar), return; end  
+    [depvar,depsel] = get_variable(mobj,promptxt,'XYZmxvar',1);
+    if isempty(depvar), return; end  
     isvalid = checkdimensions(indvar.data,depvar.data);
     if ~isvalid, return; end
 
     if strcmp(answer,'3D')
         promptxt = 'Select variable to scale scatter points:';    
-        scalevar = get_variable(mobj,promptxt);
+        scalevar = get_variable(mobj,promptxt,'XYZmxvar',1);
         if isempty(scalevar), return; end 
         isvalid = checkdimensions(indvar.data,scalevar.data);
         if ~isvalid, return; end
@@ -76,29 +77,48 @@ function scatter_plot(mobj)
     %desc - variable description, case - case description
     hf = figure('Tag','UserFig');    
     ax = axes (hf);
-    legtxt = sprintf('%s(%s)',depvar.name,indvar.name);
+    legtxt = sprintf('%s(%s)',depvar.desc,indvar.desc);    
     hs = scatter(ax,indvar.data,depvar.data,markersz,'filled','DisplayName',legtxt);
     xlabel(indvar.label)
-    if strcmp(indvar.scale,'Log')
+    if strcmp(indsel.scale,'Log')
         idx = find(mod(ax.XTick(:), 1) == 0);
         ax.XTick = ax.XTick(idx); %remove non integer exponents
         ax.XTickLabel = cellstr(num2str(ax.XTick(:), '10^{%d}'));
     end
     ylabel(depvar.label)
-    if strcmp(depvar.scale,'Log')
+    if strcmp(depsel.scale,'Log')
         idy = find(mod(ax.YTick(:), 1) == 0);
         ax.YTick = ax.YTick(idy);  %remove non integer exponents
         ax.YTickLabel = cellstr(num2str(ax.YTick(:), '10^{%d}'));
     end
-    
-
+   
     legend('Location','best')
-    title(sprintf('%s v %s',depvar.desc,indvar.desc))
+    seltxtX = get_selection_text(indvar,6,'X'); %full description of dimensions
+    titxtX = get_selection_text(indvar,5,'X');  %short description of dimensions
+
+    seltxtY = get_selection_text(depvar,6,'Y'); %full description of dimensions
+    titxtY = get_selection_text(depvar,5,'Y');  %short description of dimensions            
+                
+    seltxt = sprintf('%s\n%s',seltxtX,seltxtY);         
+    title(sprintf('%s\n%s',titxtX,titxtY))
     if strcmp(answer,'3D')
         hs.MarkerFaceAlpha = 0.5; %transparency
-        stxt = sprintf('Circles scaled by %s',scalevar.desc);
-        subtitle(stxt)  
+        seltxtZ = get_selection_text(scalevar,6,'Z'); %full description of dimensions
+        titxtZ = get_selection_text(scalevar,5,'Z');  %short description of dimensions
+        seltxt = sprintf('%s\n%s',seltxt,seltxtZ);
+        subtitle(titxtZ)
     end
+    
+    %create button to allow user to view detailed selection description
+    
+    hf.Units = 'normalized';
+    butxt = 'Selection';   %text to appear on button
+    position = [0.85,0.92,0.1,0.05];          %position of button on parent (units as per parent)
+    callback =  @(src,evt)display_selection(src,evt); %function to be called on button press
+    tag = 'selbutton';
+    tooltip = 'Details of selection made';
+    hbut = setactionbutton(hf,butxt,position,callback,tag,tooltip);
+    hbut.UserData = seltxt;
 end
 
 %%
